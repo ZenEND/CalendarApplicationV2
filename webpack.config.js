b2dff00ea83689d0d8d120c3cac5d1d4c90d3567
ZenEnd <o.s.maksimenko@gmail.com>
2019-09-07 15:05:39 +0300

    feat: gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     entry: ["@babel/polyfill","./src/index.js"],
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'index_bundle.js',
         publicPath: '/'
     },
