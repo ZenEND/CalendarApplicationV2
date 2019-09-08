c653fe7486b59f4899d9deb2c21e855efda63bb2
ZenEnd <o.s.maksimenko@gmail.com>
2019-09-08 15:03:17 +0300

    fix: modal css fixes

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -17,7 +17,7 @@ const Popup = ({popup, popupClose, events, eventAdd,eventRemove}) => {
             notice : notice.current.value,
             backgroundColor : color.current.value,
         }
-        if (popup.id!=null){
+        if (popup.id){
             eventRemove(popup.id)
             console.log("hi")
         }
